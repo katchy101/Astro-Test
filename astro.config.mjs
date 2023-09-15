ac7e1b27b9c6716b4d32c7b346f3694ff591c0b6
alexjoekatz <alex.katz@fullstack.co.za>
2023-09-15 16:20:50 +0200

    Storyblok Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
 import { defineConfig } from 'astro/config';
-
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [
+    preact(),
+    tailwind(),
+    storyblok( {
+      accessToken: env.STORYBLOK_TOKEN,
+    })
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
