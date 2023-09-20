4ba17f5d360636990acf810b546cbf6fc6dad19a
alexjoekatz <alex.katz@fullstack.co.za>
2023-09-20 12:39:02 +0200

    SSr

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
       exclude: ['js-big-decimal']
     }
   }
-  // ,
-  // output: "server",
-  // adapter:vercel()
+  ,
+  output: "server",
+  adapter:vercel()
 });
